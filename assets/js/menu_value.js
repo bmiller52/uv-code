999e418780290bbd42f3031a94671f8e1d9d53b6
bmiller52 <millerbam52@gmail.com>
2017-02-11 13:14:39 -0500

    adding js master control menu

---
+++ assets/js/menu_value.js
@@ -0,0 +1,27 @@
+
+var home = 'Home';
+document.getElementById("home_value").innerHTML = home;
+
+var post = 'Recent Posts';
+document.getElementById("post_value").innerHTML = post;
+
+var html_css = 'HTML/CSS';
+document.getElementById("html_css_value").innerHTML = html_css;
+
+var python = 'Python';
+document.getElementById("python_value").innerHTML = python;
+
+var js = 'Javascript';
+document.getElementById("js_value").innerHTML = js;
+
+var php = 'PHP';
+document.getElementById("php_value").innerHTML = php;
+
+var contact = 'Contact';
+document.getElementById("contact_value").innerHTML = contact;
+
+var team = 'Our Team';
+document.getElementById("team_value").innerHTML = team;
+
+var name = 'Ultraviolet Code';
+document.getElementById("logo_name").innerHTML = name;
